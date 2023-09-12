1b580cb76d1748fe07b92e16a2f11db8da2dc6d2
beemaster1972 <ugor.finno@yandex.ru>
2023-09-12 14:58:11 +0500

    2.8.2.1

---
+++ 2.8.2.1.py
@@ -0,0 +1,19 @@
+import time
+from functools import lru_cache
+
+# Функция для вычисления факториала
+@lru_cache()
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+start_time = time.time()
+
+# Вычисление факториала для каждого числа от 1 до 20
+for i in range(1, 21):
+    print(f'Факториал {i} = {factorial(i)}')  # Выводим факториал числа
+
+end_time = time.time()
+print(f'Вычислено 20 факториалов за {end_time - start_time} секунд')  # Выводим общее время вычисления
